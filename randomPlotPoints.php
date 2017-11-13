c6ee83543cfa264b04dca0aa8b4e88faa881c6e3
MauriceKeigtley <maurice.keightley@gmail.com>
2017-11-13 16:28:14 +1000

    Add files via upload
    random plotpoints working with 100 plots in array.

--- randomPlotPoints.php
+++ randomPlotPoints.php
@@ -10,51 +10,35 @@ Program layout - english
 
 */
 
-//init variables
-$newPlotPoint_x =0;
-$newPlotPoint_y = 0;
-$plotPoint_array = [];
+	//init variables
+	$newPlotPoint_x =0;
+	$newPlotPoint_y = 0;
+	$plotPoint_array = [];
 
-//how many dots we have so far?
-while(count($plotPoint_array)<100){
-	//Get x and y points
-	$newPlotPoint_x = rand(0,1000);
-	$newPlotPoint_y = rand(0,1000);
-	$plot_status=false;
-	
-	//check array
-	foreach ($plotPoint_array as $plotPoint){
-		//check if plot exists
-		if($plotPoint[0]==$newPlotPoint_x&&$plotPoint[1]==$newPlotPoint_y){
-			//if found - ignore entry
-			$plot_status=true;
+	//how many dots we have so far?
+	while(count($plotPoint_array)<100){
+		//Get x and y points
+		$newPlotPoint_x = rand(0,1000);
+		$newPlotPoint_y = rand(0,1000);
+		$plot_status=false;
+		
+		//check array
+		foreach ($plotPoint_array as $plotPoint){
+			//check if plot exists
+			if($plotPoint[0]==$newPlotPoint_x&&$plotPoint[1]==$newPlotPoint_y){
+				//if found - ignore entry
+				$plot_status=true;
+			}
+		}
+		
+		//if plot points are not found - insert them into array
+		if(!$plot_status){
+			array_push($plotPoint_array,[$newPlotPoint_x,$newPlotPoint_y]);
+			$plotPoint_array++;	
 		}
 	}
-		
-	print_r("test");
-	exit();
-	/*
-	else{
-			//Not found - insert new plot point.
-			array_push($plotPoint_array,[$newPlotPoint_x,$newPlotPoint_y]);
-			$numberOfPoints++;
-		}
-		*/
-	$plotPoint_array++;	
-}
-
-
-
-	
 	print_r($plotPoint_array);
-	exit();
-
-
-
-
-
-
-
-
 
 ?>
+
+
