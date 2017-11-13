31d6a50fbe78c1a50e34624585ac8ce1fb3170e1
MauriceKeigtley <maurice.keightley@gmail.com>
2017-11-13 16:47:52 +1000

    Add files via upload
    Made the script into a class- can be called locally now to create list.

--- randomPlotPoints.php
+++ randomPlotPoints.php
@@ -9,35 +9,49 @@ Program layout - english
 -	if not - add to list
 
 */
+define("MIN_GRID", 0);
+define("MAX_GRID", 1000);
+define("MAX_PLOTS", 100);
 
-	//init variables
-	$newPlotPoint_x =0;
-	$newPlotPoint_y = 0;
-	$plotPoint_array = [];
+class RandomPlotPoints{
 
-	//how many dots we have so far?
-	while(count($plotPoint_array)<100){
-		//Get x and y points
-		$newPlotPoint_x = rand(0,1000);
-		$newPlotPoint_y = rand(0,1000);
-		$plot_status=false;
-		
-		//check array
-		foreach ($plotPoint_array as $plotPoint){
-			//check if plot exists
-			if($plotPoint[0]==$newPlotPoint_x&&$plotPoint[1]==$newPlotPoint_y){
-				//if found - ignore entry
-				$plot_status=true;
+	function generatePoints(){
+		//init variables
+		$newPlotPoint_x =0;
+		$newPlotPoint_y = 0;
+		$plotPoint_array = [];
+
+		//how many dots we have so far?
+		while(count($plotPoint_array)<MAX_PLOTS){
+			//Get x and y points
+			$newPlotPoint_x = rand(MIN_GRID,MAX_GRID);
+			$newPlotPoint_y = rand(MIN_GRID,MAX_GRID);
+			$plot_status=false;
+			
+			//check array
+			foreach ($plotPoint_array as $plotPoint){
+				//check if plot exists
+				if($plotPoint[0]==$newPlotPoint_x&&$plotPoint[1]==$newPlotPoint_y){
+					//if found - ignore entry
+					$plot_status=true;
+				}
+			}
+			
+			//if plot points are not found - insert them into array
+			if(!$plot_status){
+				array_push($plotPoint_array,[$newPlotPoint_x,$newPlotPoint_y]);
+				$plotPoint_array++;	
 			}
 		}
-		
-		//if plot points are not found - insert them into array
-		if(!$plot_status){
-			array_push($plotPoint_array,[$newPlotPoint_x,$newPlotPoint_y]);
-			$plotPoint_array++;	
-		}
+		print_r($plotPoint_array);
 	}
-	print_r($plotPoint_array);
+
+}
+
+
+$RandomPlotPoints = new RandomPlotPoints();
+$RandomPlotPoints->generatePoints();
+
 
 ?>
 
