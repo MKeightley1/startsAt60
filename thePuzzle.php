2e5a5db586a72f3956cd0753402a7ab97fb4ea36
MauriceKeigtley <maurice.keightley@gmail.com>
2017-11-13 17:50:52 +1000

    Add files via upload
    Added task 2 = with returning grid points counter that are not in circle.

--- thePuzzle.php
+++ thePuzzle.php
@@ -1,11 +1,21 @@
 <?php
+/*
+
+*/
+
+
 include 'randomPlotPoints.php';
+include 'checkCircleRadius.php';
 
 
-
+//collect all 100 points on graph
 $RandomPlotPoints = new RandomPlotPoints();
 $plotPoints = $RandomPlotPoints->generatePoints();
 print_r($plotPoints);
+
+$CheckCircleRadius = new CheckCircleRadius();
+print_r($CheckCircleRadius->checkPoints($plotPoints));
+
 
 ?>
 
